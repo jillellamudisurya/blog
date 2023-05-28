4214eafde337bedf1fdc7b082ed15391665bcd2b
Surya Jillellamudi <suryajillellamudi@gmail.com>
2023-05-28 21:32:20 +0530

    Created Card component 1. Reusable cards component 2. We can add unique styling for each cards as I am passing className as props

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,16 @@
+import AboutCard from "./components/card";
+import Styles from "./style.module.css";
+
 export default function About() {
   return (
     <main>
       <h1>About</h1>
-      <p>Hello World</p>
+      <div className={Styles.cardsContainer}>
+        <AboutCard className={Styles.card1} />
+        <AboutCard className={Styles.card2} />
+        <AboutCard className={Styles.card3} />
+        <AboutCard className={Styles.card4} />
+      </div>
     </main>
   );
 }
